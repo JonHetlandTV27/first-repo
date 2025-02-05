02fd28688472aaa71f62170b979ca4ec086e6914
JonHetlandTV27 <joonhetland@gmail.com>
2025-02-05 10:06:31 -0700

    added better code

--- boxPrint.py
+++ boxPrint.py
@@ -1,8 +1,30 @@
 def boxPrint(string):
     words=string.split()
-    print('**********')
+    highest_length=0
     for word in words:
-        print('* '+ word + ' *')
-    print('**********')
+        word_length=0
+        for letter in word:
+            word_length+=1
+        if word_length>highest_length:
+            highest_length=word_length
+    
+    top_box=[]
+    for i in range(2+highest_length):
+        top_box.append('*')
+    top_string="".join(top_box)
+    
+    print(top_string)
+    for word in words:
+        spaces=highest_length
+        space_string=[]
+        
+        for letter in word:
+            spaces-=1
+        for i in range(spaces):
+            space_string.append(' ')
+        space_string1= "".join(space_string)
+        print('*' + word + space_string1 + '*')
+    print(top_string)
+boxPrint(input('string here '))
 
-boxPrint(input('what string would you like to be input?'))
+
